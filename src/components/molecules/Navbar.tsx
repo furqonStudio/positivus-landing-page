29e9acf02817e372cda0955e74f40a5bf5fa6834
furqonstudio <furqonstudio@gmail.com>
2024-10-17 14:58:53 +0700

    feat: fix navbar padding in large screen

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from '../atoms/Logo'
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between items-center px-5 mt-8">
+    <div className="flex flex-row justify-between items-center px-5 mt-8 lg:px-24">
       <div className="w-[144px] md:w-fit">
         <Logo />
       </div>
